b9445262edc96522f24693d35f8229316e9dc71c
Batmend Ganbaatar <batmend@gege.mn>
2025-04-27 18:27:10 +0800

    linter happy

--- lib/widgets/reports/category_report_view.dart
+++ lib/widgets/reports/category_report_view.dart
@@ -1,9 +1,10 @@
+import "package:flow/reports/category_flow_report.dart";
 import "package:flutter/material.dart";
 
 class CategoryReportView extends StatelessWidget {
-  final CategoryReport report;
+  final CategoryFlowReport report;
 
-  const CategoryReportView({super.key, this.report});
+  const CategoryReportView({super.key, required this.report});
 
   @override
   Widget build(BuildContext context) {
