7f5dc71dcccf5279b0ed7d8f3440a812962ce6bb
Batmend Ganbaatar <batmend@gege.mn>
2025-10-31 14:28:14 +0800

    fix setup default account currencies,  setup for ios widgets

---
+++ ios/Flow Widgets/Flow_WidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  Flow_WidgetsBundle.swift
+//  Flow Widgets
+//
+//  Created by Batmend Ganbaatar on 2025.10.24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Flow_WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Flow_Widgets()
+        Flow_WidgetsControl()
+    }
+}
